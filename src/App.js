b07c14482b81df7aca3ea3722de0f293a08a18ae
AmrMaree <amr.maree14@gmail.com>
2024-09-12 02:45:28 +0200

    final commit in the project

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react';
 import Header from './components/Header';
 import AddTask from './components/AddTask';
 import TaskList from './components/TaskList';
-import './App.css';
 
 function App() {
   const [taskList, setTaskList] = useState(JSON.parse(localStorage.getItem("tasklist")) || []);
